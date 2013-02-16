bd2f36839e2a871f14d43af6e1b9367e9118ba83
Antony Liang <findaliang@gmail.com>
2013-02-16 15:38:16 -0500

    Worked with audio clips

--- SandBox/Assets/DogBehavior.cs
+++ SandBox/Assets/DogBehavior.cs
@@ -7,7 +7,8 @@ public RaycastHit hit;
 public Vector3 one, pos;
 public static bool makeGUI;
 private bool makeGUIflag;
-	
+private int options;
+public AudioClip [] myAudioClips;
 	// Use this for initialization
 	void Start () {	
 		one = transform.TransformDirection(Vector3.one);
@@ -15,11 +16,28 @@ private bool makeGUIflag;
 		ray = new Ray(pos, one);
 		makeGUI = false;
 		makeGUIflag = false;
+		options = 1;
 	}
 	
 	void OnGUI(){
 		if(makeGUI){
-			GUI.Box(new Rect(250,200,600,100), "\n\nPress <Spacebar> to ask the robot to do your homework");
+			GUI.Box(new Rect(250,200,600,130), "\nDo my homework \n\npantpantpantpantpantpantpant \n\nfuck this I'm going home");
+		}
+	}
+	
+	void cycleOptions(){
+		if(options < 1)
+			options = 1;
+		if(options > 3)
+			options = 3;
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+			options--;
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+			options++;
+		if(Input.GetKeyDown(KeyCode.Space)){		
+			//audio.clip = myAudioClips[0];
+			//audio.Play();
+			print (myAudioClips.length);
 		}
 	}
 	
@@ -31,10 +49,10 @@ private bool makeGUIflag;
 			makeGUI = true;;
 			makeGUIflag = true;
 		}
-		if((Input.GetKeyDown (KeyCode.Space)) && (makeGUI)){
-			makeGUI = false;
-			audio.Play ();
-			
+		if(makeGUI){
+			cycleOptions();
 		}
 	}
 }
+					//makeGUI = false;
+					//audio.Play ();
